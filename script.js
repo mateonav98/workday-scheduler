3d91aa5a980ab06b4f070a4007d20256455624c8
mateonav98 <mateonav98@gmail.com>
2022-04-07 17:16:07 -0700

    adding local storage

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 // Pseudocode: GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
-// THEN I am presented with timeblocks for standard business hours
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
@@ -18,5 +15,52 @@
 //Current day displaying at top of header
 var date =  moment().format("[Hello, Today is] dddd, MMMM Do YYYY");
 $("#currentDay").text(date)
-console.log("hello", date)
+console.log(date)
 
+
+var currentTime = moment().format('H');
+console.log()
+
+// $('.form-control')
+//   .each(function(){
+//     var val = parseInt($(this).prop('id'));
+//     if(val > currentTime && val < currentTime + 12){
+//       $(this).css('background-color','green');
+//     }else if(val < currentTime && val > currentTime-12){
+//       $(this).css('background-color','Gray');
+//     }else if(val === currentTime){
+//       $(this).css('background-color','red');
+//     }else{
+//       $(this).css('background-color','White');
+//     }
+//   });
+
+// localStorage.setItem()
+
+var saveBtn = document.querySelector("#button-addon2");
+var text = document.querySelector("#text")
+
+function getTextData() {
+var text = localStorage.getItem("text");
+console.log(text)
+}
+
+saveBtn.addEventListener("click", function() {
+    var text = document.querySelector("#text").value
+    text.textContent=text;
+    localStorage.setItem("text", text);
+    getTextData()
+})
+
+
+
+// function getTextData() {
+//     localStorage['text']=document.getElementById('text').value;
+//     console.log()
+// }
+// getTextData()
+// function addTextData() {
+//     document.getElementById('text').value = localStorage['text']
+//     console.log()
+// }
+// addTextData()
